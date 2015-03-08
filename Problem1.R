26d4165f2053fa2eab0465329fae3094939909f7
Syeda Kauser Inamdar <skinamdar@ucdavis.edu>
2015-03-08 11:42:43 -0700

    Finished part B

--- Problem1.R
+++ Problem1.R
@@ -13,8 +13,31 @@ Problem1 <- function()
   SE <- sqrt(((s1 ^2) / length(s1)) + ((s2 ^ 2) / length(s2)))
   CI_2means <- c(Difference - 1.96 * SE, Difference + 1.96 * SE)
   # cat(c(month,X_bar,Y_bar,Difference,s1,s2,SE,CI_2means))
+  
+  cat("Confidence Interval for Difference between 2 population means of bike rentals in March from 2011 to 2012")
+  cat("\n")
   cat(CI_2means)
+  cat("\n")
   
   # part B
+  tempMonthYear0 <- day$cnt[(day$yr == 0) & (day$mnth == month) & (day$temp >.3)]
+  tempMonthYear1 <- day$cnt[(day$yr == 1) & (day$mnth == month) & (day$temp > .3)]
+ 
+  n0<-length(monthYear0)
+  n1<-length(monthYear1)
+  
+  p_hat0<-length(tempMonthYear0)/n0
+  p_hat1<-length(tempMonthYear1)/n1
+  
+  Difference2<-p_hat0-p_hat1
+  
+  SE2<-sqrt((p_hat0*(1-p_hat0)/n0)+(p_hat1*(1-p_hat1)/n1))
+  CI_2props<-c(Difference2-1.96*SE2,Difference2+1.96*SE2)
+  
+  cat("Confidence Interval for Difference between 2 population proportions of days in which \n the temperature in March was greater than .3 from 2011 to 2012")
+  cat("\n")
+  cat(CI_2props)
+  cat("\n")
+  
   
 } # end Problem1
