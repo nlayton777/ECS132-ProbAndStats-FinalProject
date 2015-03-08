182684673f47c0a9bd3d165d19f917d74f3229fe
Nick Layton <nllayton@ucdavis.edu>
2015-03-08 11:02:30 -0700

    change file path in Problems 2 and 3

--- Problem1.R
+++ Problem1.R
@@ -1,5 +1,6 @@
 Problem1 <- function()
 {
+  # part A
   day <- read.csv(file="day.csv",header=TRUE,sep=",")
   Jan2011 <- day$cnt[(day$yr == 0) & (day$mnth == 1)]
   Jan2012 <- day$cnt[(day$yr == 1) & (day$mnth == 1)]
@@ -10,5 +11,5 @@ Problem1 <- function()
   s2 <- sd(Jan2012)
   SE <- sqrt(((s1 ^2) / length(s1)) + ((s2 ^ 2) / length(s2)))
   CI_2means <- c(Difference - 1.96 * SE, Difference + 1.96 * SE)
-  return (CI)
+  return (c(X_bar,Y_bar,Difference,s1,s2,SE,CI_2means))
 } # end Problem1
--- Problem3.R
+++ Problem3.R
@@ -1,5 +1,5 @@
 Problem3 <- function()
 {
-    day <- read.csv(file="Data/day.csv",header=TRUE,sep=",")
+    day <- read.csv(file="day.csv",header=TRUE,sep=",")
     return (day)
 } # end Problem3
