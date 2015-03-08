0ed901de60e7acf9d1097d871ec32202d55818a9
Nick Layton <nllayton@ucdavis.edu>
2015-03-08 11:19:23 -0700

    finalize our month to be March

--- Problem1.R
+++ Problem1.R
@@ -2,14 +2,19 @@ Problem1 <- function()
 {
   # part A
   day <- read.csv(file="day.csv",header=TRUE,sep=",")
-  Jan2011 <- day$cnt[(day$yr == 0) & (day$mnth == 1)]
-  Jan2012 <- day$cnt[(day$yr == 1) & (day$mnth == 1)]
-  X_bar <- mean(Jan2011)
-  Y_bar <- mean(Jan2012)
+  month <- 3
+  monthYear0 <- day$cnt[(day$yr == 0) & (day$mnth == month)]
+  monthYear1 <- day$cnt[(day$yr == 1) & (day$mnth == month)]
+  X_bar <- mean(monthYear0)
+  Y_bar <- mean(monthYear1)
   Difference <- X_bar - Y_bar
-  s1 <- sd(Jan2011)
-  s2 <- sd(Jan2012)
+  s1 <- sd(monthYear0)
+  s2 <- sd(monthYear1)
   SE <- sqrt(((s1 ^2) / length(s1)) + ((s2 ^ 2) / length(s2)))
   CI_2means <- c(Difference - 1.96 * SE, Difference + 1.96 * SE)
-  return (c(X_bar,Y_bar,Difference,s1,s2,SE,CI_2means))
+  # cat(c(month,X_bar,Y_bar,Difference,s1,s2,SE,CI_2means))
+  cat(CI_2means)
+  
+  # part B
+  
 } # end Problem1
