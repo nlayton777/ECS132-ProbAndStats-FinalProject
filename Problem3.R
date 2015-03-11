57b7e8a1742b37c56ce3eda4ac4924d6922ad9f9
Nick Layton <nllayton@ucdavis.edu>
2015-03-11 09:48:49 -0700

    rename file

--- Problem3.R
+++ Problem3.R
@@ -3,12 +3,14 @@ library(ggplot2)
 PartA <- function()
 {
     day <- read.csv(file="day.csv",header=TRUE,sep=",")
-    p <- ggplot(day) + geom_histogram(aes(temp)) 
-    p <- p + labs(title="Daily Temperate Frequency", x="Temperature",y="Frequency") 
-    p <- p + theme(title=element_text(size=3),text=element_text(size=3),axis.ticks=element_line(size=1)) 
+    p <- ggplot(day)
+    p <- p + geom_histogram(aes(temp)) 
+    #p <- p + labs(title="Daily Temperature Frequency", x="Temperature",y="Frequency")
+    p <- p + xlab("Temperature") + ylab("Frequency") + ggtitle("Daily Temperature Distribution")
+    p <- p + theme(title=element_text(size=12),text=element_text(size=12),axis.ticks=element_line(size=2)) 
     print(p)
-    ggsave("Problem3A.pdf",width=1,height=1.5,plot=last_plot())
-    dev.off()
+    ggsave("Problem3A.pdf",width=5,height=7,plot=last_plot())
+    #dev.off()
 } # PartA
 
 PartB <- function()
@@ -19,4 +21,5 @@ PartB <- function()
 PartC <- function()
 {
   day <- read.csv(file="day.csv",header=TRUE,sep=",")
+
 } # PartC
