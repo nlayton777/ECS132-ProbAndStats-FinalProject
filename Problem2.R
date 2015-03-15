e702bdea6d6d50abccaa567bd331a62fd32f410d
Wotwell <Wjotwell@ucdavis.edu>
2015-03-14 21:08:29 -0700

    I thought I commited everthing. If this is my only commit then I am fixing part b

--- Problem2.R
+++ Problem2.R
@@ -40,6 +40,25 @@ PartD <- function()
 
 PartE <- function()
 {
+  day <- read.csv(file="day.csv",header=TRUE,sep=",")
+  nr <- nrow(day)
+  #training and validation are indexes into our data
+  training <- sample(1:nr, ceiling(2*nr/3), replace=FALSE)
+  validation <- setdiff(1:nr, training)
+  
+  #modelling count based on 
+  # "season"     "yr"    "mnth"    
+    # "holiday"        "workingday"
+  # "weathersit" "temp"       "atemp"      "hum"       
+  # "windspeed"
+  trainingModel <- lm(day[training, 16] ~ .,day[training, c(3:6, 8:13)])
+  print(summary(trainingModel))
+  prediction <- predict(trainingModel,newdata=day[validation, c(3:6, 8:13)])
+  difference <- day[validation,16] - prediction
+  
+  diff <- c(mean(abs(difference)), min(abs(difference)), max(abs(difference)))
+  names(diff) <- c("mean", "min", "max")
+  return(diff)  
   
 } # PartE()
 
